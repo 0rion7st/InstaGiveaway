360342da153d12b765cc28c66a8238da8316c226
0rion7st <orionishe@gmail.com>
2015-03-21 13:57:12 +0200

    Minor changes

--- www/js/services.js
+++ www/js/services.js
@@ -321,9 +321,9 @@ angular.module('giveaways.services', ['ngResource'])
             generateHashTag : function()
             {
                 var text = "";
-                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-                for( var i=0; i < 6; i++ )
+                for( var i=0; i < 8; i++ )
                     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
                 return "ww"+text
